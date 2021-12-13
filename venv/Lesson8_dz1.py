7af25e78f4509a6179290fdc9ab5ee20a77226e8
Ivan <ivansyn2013@yandex.ru>
2021-12-13 18:24:27 +0300

    init commit

---
+++ venv/Lesson8_dz1.py
@@ -0,0 +1,13 @@
+import time
+class Trafic_light:
+    color_list = ['\033[41m{}', '\033[43m{}', '\033[42m{}']
+    __color = '\033[41m{}'
+
+    def running(self):
+        x_t = 7
+        for col in Trafic_light.color_list:
+            print('{}}'.format(col), end='')
+            time.sleep(x_t)
+            x_t = 2
+a = Trafic_light()
+a.running()
