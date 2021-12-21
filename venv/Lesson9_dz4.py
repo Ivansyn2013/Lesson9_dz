34fb3e17e917c84b61d321ea6d2a5a78e7da77a5
ivan <ivansyn2013@yandex.ru>
2021-12-21 22:02:53 +0300

    ok

--- venv/Lesson9_dz4.py
+++ venv/Lesson9_dz4.py
@@ -1,26 +1,28 @@
 class Car:
     is_police = False
+
     def __init__(self, name, color):
         self.name = name
         self.color = color
         self.speed = 0
 
-    def go(self, speed = 35):
+    def go(self, speed=35):
         self.speed = speed
         print(f'Car {self.name} is going. Speed is {self.speed}')
 
-    def stop(self, speed = 0):
+    def stop(self, speed=0):
         self.speed = 0
         print(f' Car {self.name} is stoping. Speed is {self.speed}')
 
-    def turn(self,direction):
+    def turn(self, direction):
         if direction == 'left' or direction == 'right':
             print(f' Car {self.name} turned on {direction}')
-        else :
+        else:
             print(f' Car dont turn')
 
     def show_speed(self):
         print(f'Car speed is {self.speed}')
+
 
 class TownCar(Car):
 
@@ -34,6 +36,7 @@ class TownCar(Car):
 class SportCar(Car):
     pass
 
+
 class WorkCar(Car):
     def show_speed(self):
         if self.speed >= 41:
@@ -41,19 +44,20 @@ class WorkCar(Car):
         else:
             print(f' Car {self.name} speed is {self.speed}')
 
+
 class PoliceCar(Car):
     is_police = True
 
 
 auto_1 = PoliceCar('white', 'BMW')
 auto_1.go(20)
-auto_2 = SportCar('red','Mersedes')
+auto_2 = SportCar('red', 'Mersedes')
 auto_2.go(80)
 auto_2.show_speed()
 auto_3 = WorkCar('black', 'Nissan')
 auto_3.go(60)
 auto_3.show_speed()
-auto_4 = TownCar('green','lexus')
+auto_4 = TownCar('green', 'lexus')
 auto_4.go(65)
 auto_4.show_speed()
 auto_4.stop()
